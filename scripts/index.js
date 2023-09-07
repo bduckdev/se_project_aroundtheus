104386811a93027520db7bd6896621dcba70aa6c
Brennan Duck <brennantduck@gmail.com>
2023-09-07 13:59:14 -0400

    added array for card images

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const initialCards = [
+  {
+    title: "Yosemite Valley",
+    image:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    title: "Lake Louise",
+    image:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    title: "Bald Mountains",
+    image:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    title: "Latemar",
+    image:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    title: "Vanoise National Park",
+    image:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    title: "Lago di Braies",
+    image:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+console.log(initialCards);
