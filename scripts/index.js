5e087971dffc26934618bcf16a0240257c47f1d7
Brennan Duck <brennantduck@gmail.com>
2023-09-14 15:33:29 -0400

    Added modal edit window functionality

--- scripts/index.js
+++ scripts/index.js
@@ -25,15 +25,40 @@ const initialCards = [
   },
 ];
 
+/* ELEMENTS */
+
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseButton = document.querySelector("#profile-edit-close");
+const profileName = document.querySelector("#profile-name");
+const profileDescription = document.querySelector("#profile-description");
+const profileNameInput = document.querySelector("#profile-name-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector("#profile-edit-form");
+
+/* event handlers */
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileName.innerText = profileNameInput.value;
+  profileDescription.innerText = profileDescriptionInput.value;
+  closePopup();
+}
+
+/* event listeners */
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
 
 profileEditButton.addEventListener("click", () => {
+  profileNameInput.value = profileName.innerText;
+  profileDescriptionInput.value = profileDescription.innerText;
   profileEditModal.classList.add("modal_opened");
 });
 
-const profileEditCloseButton = document.querySelector("#profile-edit-close");
+profileEditCloseButton.addEventListener("click", closePopup);
 
-profileEditCloseButton.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
-});
+profileEditModal.addEventListener("submit", handleProfileEditSubmit);
