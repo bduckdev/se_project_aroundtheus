87350a556a6dad958e10e81ae10d52e4ec010035
Brennan Duck <bduck@Brennans-MacBook-Pro.local>
2023-12-22 19:46:20 -0500

    added and partially implemented section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,13 +1,3 @@
-/*
-    It has a public method named renderItems()
-    that renders all eleooments on the page.
-
-    It should iterate through the items array
-and call the renderer() function on each item.
-
-    This method should be called once on page load.
-    */
-
 export default class Section {
   constructor({ items, renderer }, selector) {
     this.items = items;
